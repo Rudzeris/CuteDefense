e08295879e2446c2c16ce99560ef121d9fab0b55
Rudzeris <ruzil1atangulov@gmail.com>
2025-05-21 02:05:29 +1000

    CreateDamageAttack

---
+++ Assets/Scripts/GameObjects/Attack/DamageAttack.cs
@@ -0,0 +1,37 @@
+﻿using Assets.Scripts.GameObjects.Fractions;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.GameObjects
+{
+    public class DamageAttack : BasicAttack
+    {
+        public override event Action OnAttacking;
+        public override event Action<bool> OnViewEnemy;
+
+        [SerializeField] private int damage;
+        public int Damage => damage;
+
+        protected override IEnumerator Attack()
+        {
+            GameObject enemy = null;
+            while (enemy == null)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, DistanceAttack, gameObject.layer);
+
+                if (hit.collider != null && hit.collider?.GetComponent<IFraction>()?.Fraction != this.Fraction.Fraction)
+                {
+                    OnViewEnemy?.Invoke(true);
+                    if (hit.collider?.GetComponent<IBasicEntity>() is IBasicEntity entity && CheckEntity(entity))
+                    {
+                        entity.TakeDamage(Damage);
+                        OnAttacking?.Invoke();
+                    }
+                }
+
+                yield return null;
+            }
+        }
+    }
+}
