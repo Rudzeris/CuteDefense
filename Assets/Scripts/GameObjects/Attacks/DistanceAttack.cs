cc8371627032a50f04f6c0633040bfa41b427910
Rudzeris <ruzil1atangulov@gmail.com>
2025-05-24 13:51:44 +1000

    Добавил DistanceAttack

---
+++ Assets/Scripts/GameObjects/Attacks/DistanceAttack.cs
@@ -0,0 +1,70 @@
+﻿using Assets.Scripts.GameObjects.Entities;
+using Assets.Scripts.GameObjects.Fractions;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.GameObjects.Attacks
+{
+    public class DistanceAttack : AttackWithTime
+    {
+        public override event Action OnAttacking;
+        public override event Action<bool> OnViewEnemy;
+        public Vector3 BulletSpawnPoint;
+        public GameObject Bullet;
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(BulletSpawnPoint, new Vector3(transform.position.x + DistanceAttack * Mathf.Sign(transform.localScale.x)
+                , transform.position.y, transform.position.z));
+        }
+        protected override IEnumerator Attack()
+        {
+            // Ищет противника в одной линии и атакует
+            IBasicEntity enemyEntity = null;
+            while (IsActive)
+            {
+                if (enemyEntity == null)
+                {
+                    RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, Mathf.Sign(transform.localScale.x) == -1 ? Vector2.left : Vector2.right, DistanceAttack);
+
+                    enemyEntity = null;
+                    foreach (RaycastHit2D hit in hits)
+                    {
+                        if (hit.collider != null && (hit.collider.GetComponent<IFraction>()?.Fraction ?? this.Fraction.Fraction) != this.Fraction.Fraction)
+                        {
+                            enemyEntity = hit.collider.GetComponent<IBasicEntity>();
+                            if (enemyEntity != null && CheckEntity(enemyEntity))
+                            {
+                                break;
+                            }
+                            else enemyEntity = null;
+                        }
+                    }
+                    OnViewEnemy?.Invoke(enemyEntity != null);
+
+                    if (enemyEntity != null) // Если спереди челик, то 1-я атака ждет
+                        yield return new WaitForSeconds(ForAllFirstAttackCooldown);
+                }
+                if (enemyEntity != null)
+                {
+                    GameObject obj = Instantiate(Bullet, BulletSpawnPoint, Quaternion.identity);
+                    if (obj != null)
+                    {
+                        obj.transform.parent = null;
+                        OnAttacking?.Invoke();
+                        enemyEntity = null;
+
+                        // TODO: Настроить Bullet
+
+                        yield return new WaitForSeconds(Cooldown);
+                    }
+                    else
+                        yield return null;
+                }
+                else
+                    yield return null;
+            }
+        }
+    }
+}
